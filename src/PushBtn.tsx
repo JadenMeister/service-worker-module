38dc6da031b9f328c6b80aa85b3a5f6d3f854cf0
Jadenmeister <bigdragon00@naver.com>
2025-08-21 14:09:11 +0900

    권한 관리 로직 수정

--- src/PushBtn.tsx
+++ src/PushBtn.tsx
@@ -10,7 +10,7 @@ const PushBtn = () => {
 
         console.log("현재 알림 권한:", Notification.permission);
 
-        if (Notification.permission !== "granted" || Notification.permission !== "default") {
+        if (Notification.permission !== "granted" ) {
             console.error("알림 권한이 없습니다.");
             console.log("현재 알림 권한:", Notification.permission);
             return;
@@ -20,15 +20,19 @@ const PushBtn = () => {
 
         console.log("권한체크 통과")
 
+        setTimeout(() => {
+            registration.showNotification('테스트 알림', {
+                body: '이것은 테스트 알림입니다.',
+                tag: 'test',
+                actions: [
+                    {action: 'open', title: '열기'},
+                    {action: 'close', title: '닫기'}
+                ]
+            });
+        },5000)
 
-        await registration.showNotification('테스트 알림', {
-            body: '이것은 테스트 알림입니다.',
-            tag: 'test',
-            actions: [
-                {action: 'open', title: '열기'},
-                {action: 'close', title: '닫기'}
-            ]
-        });
+
+
     };
 
     const  sendEvent = async ()=>{
